e7d42d42d5d560ea40ef180636eb6842ffc2d467
ArmstrongWall <160111223wzq@gmail.com>
2017-06-15 09:06:21 +0800

    构建CMake管理工程代码

---
+++ master/build/CMakeFiles/crab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/crab.dir/src/main.c.o"
+  "crab.pdb"
+  "crab"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/crab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ master/build/CMakeFiles/serial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/serial.dir/src/serial/serial.c.o"
+  "libserial.pdb"
+  "libserial.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/serial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
